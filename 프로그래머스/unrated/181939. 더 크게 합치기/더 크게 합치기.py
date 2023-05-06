7e79ce3ac4b533610fe27cc690dcac424384f5ad
ii_ruatnec <104310191+SUNGMYEONGGI@users.noreply.github.com>
2023-05-06 13:14:37 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,5 @@
+def solution(a, b):
+    if int(str(a) + str(b)) > int(str(b) + str(a)) or int(str(a) + str(b)) == int(str(b) + str(a)):
+        return int(str(a) + str(b))
+    elif int(str(a) + str(b)) < int(str(b) + str(a)):
+        return int(str(b) + str(a))
