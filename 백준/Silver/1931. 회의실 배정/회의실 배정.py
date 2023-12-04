764581c1b7705e437dd2638cc487fbd0691e7bb9
ii_ruatnec <104310191+SUNGMYEONGGI@users.noreply.github.com>
2023-12-04 14:28:08 +0900

    [Silver I] Title: 회의실 배정, Time: 3996 ms, Memory: 47316 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,24 +1,18 @@
-N = int(input())
-meet_data = []
+n = int(input())
+data = []
 
-for _ in range(N):
-    mt1, mt2 = map(int, input().split())
-    k = (mt1, mt2)
-    meet_data.append(k)
+for _ in range(n):
+    time = map(int, input().split())
+    data.append(tuple(time))
 
-# x[0]을 우선 정렬    
-meet_ascend_data = sorted(meet_data, key=lambda x:x[0])
-# 정렬된 data를 다시 x[1]을 기준으로 정렬
-meet_ascend_data = sorted(meet_ascend_data, key=lambda x:x[1])
+Sort_StartTime = sorted(data, key=lambda x: x[0])
+Sort_EndTime = sorted(Sort_StartTime, key=lambda x: x[1])
 
-starttime = 0
 endtime = 0
-meetable_data = []
+cnt = 0
+for i in Sort_EndTime:
+    if i[0] >= endtime:
+        endtime = i[1]
+        cnt += 1
 
-for i in range(len(meet_ascend_data)):
-    if endtime <= meet_ascend_data[i][0]:
-        starttime = meet_ascend_data[i][0]
-        endtime = meet_ascend_data[i][1]
-        meetable_data.append(meet_ascend_data[i])
-
-print(len(meetable_data))
+print(cnt)
