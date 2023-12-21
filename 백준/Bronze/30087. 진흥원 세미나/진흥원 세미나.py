b6e4343538b0927d60625b02add49b97e089a4aa
ii_ruatnec <104310191+SUNGMYEONGGI@users.noreply.github.com>
2023-12-21 23:48:09 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+n = int(input())
+for _ in range(n):
+    k = input()
+    if k == "Algorithm":
+        print("204")
+    elif k == "DataAnalysis":
+        print("207")
+    elif k == "ArtificialIntelligence":
+        print("302")
+    elif k == "CyberSecurity":
+        print("B101")
+    elif k == "Network":
+        print("303")
+    elif k == "Startup":
+        print("501")
+    elif k == "TestStrategy":
+        print("105")
