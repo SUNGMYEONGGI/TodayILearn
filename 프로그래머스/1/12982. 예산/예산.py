638803c5d3480fcd8303485e47d6f355d597599d
ii_ruatnec <104310191+SUNGMYEONGGI@users.noreply.github.com>
2024-05-25 14:19:52 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,3 +1,18 @@
+# 1. 물품을 구매할때 신청한 금액 전부 지원
+# 2. d에는 물품의 금액이 들어있는 배열
+# 3. 총 금액
+
 def solution(d, budget):
-    answer = 0
-    return answer
+    cnt = 0
+    d.sort()
+    for i in d:
+        if budget >= i:
+            budget -= i
+            cnt += 1
+            if budget == 0:
+                return cnt
+        elif budget < i:
+            return cnt
+    return cnt
+    
+    
